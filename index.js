c6a2934973cdaf00f89b9644d7620b66c42c12a3
Gowthamkurella <gowtham.kurella.12@gmail.com>
2023-12-03 13:17:24 +0530

    mongo connection check

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ app.use(session({
     saveUninitialized: true
 }));
 
+app.get("/health", async (req, res) => {
+    try {
+        // Check MongoDB connection
+        await mongoose.connection.db.admin().ping();
+        res.status(200).json({ status: 'MongoDB connection is healthy' });
+    } catch (error) {
+        console.error('MongoDB connection error during health check:', error);
+        res.status(500).json({ status: 'MongoDB connection error' });
+    }
+});
+
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
