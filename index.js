3945f73a0c32ac78fad3dbb02b112582b61c08e6
vamsimunagala <munagalavamsi37@gmail.com>
2023-12-04 21:14:10 +0530

    css routes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 // app.use(express.static(__dirname + "public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+app.use('/public', express.static('public'));
 
 const defaultSessionSecret = 'mydefaultsecretkey';
 
