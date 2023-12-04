1c54956bdb217357a70871682f08f4e5476180fa
vamsimunagala <munagalavamsi37@gmail.com>
2023-12-04 20:57:16 +0530

    added public

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 // app.use(express.static(__dirname + "public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/public', express.static('public'));
+app.use(express.static(__dirname + "/public"));
 
 const defaultSessionSecret = 'mydefaultsecretkey';
 
