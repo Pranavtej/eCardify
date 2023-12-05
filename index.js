a055406ffcd7863f259a80870359d7a06ff8b9bf
vamsimunagala <munagalavamsi37@gmail.com>
2023-12-05 10:47:12 +0530

    Ecard-2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bcrypt = require("bcrypt");
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-// app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/public', express.static('public'));
